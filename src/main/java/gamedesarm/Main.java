adedd8a24b7a9b24ea076cb5cc77d8585c257d5d
brunoerbs <brunoerbs4@gmail.com>
2024-11-25 19:16:51 -0300

    som da bomba

--- src/main/java/gamedesarm/Main.java
+++ src/main/java/gamedesarm/Main.java
@@ -3,6 +3,10 @@ package gamedesarm;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 public class Main {
     public static void main(String[] args) {
         int numeroFio = 0;
@@ -20,15 +24,21 @@ public class Main {
         } catch (BombaException be) {
             be.printStackTrace();
             System.out.println("A bomba explodiu.\nO jogo terminou.");
-            new SomExemplo();
+            try {
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(Main.class.getResource("/gamedesarm/explosion-42132.wav"));
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+                Thread.sleep(clip.getMicrosecondLength() / 1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public static void geraNumero(int numeroFio) throws BombaException {
         Random random = new Random();
         int numeroAleatorio = random.nextInt(999);
-        System.out.println("Número aleatório: " + numeroAleatorio);
-
         if (numeroAleatorio % 2 == 0) {
             throw new BombaException();
         }
